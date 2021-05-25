7c357ff8191b964fbbc16d6312f1a640bb4010a9
MasterGST Tech sols <MasterGST Tech sols@LAPTOP-9O9IFBA0>
2021-05-25 07:34:33 +0530

    SpringMvcFormApp comit in github

---
+++ src/main/java/com/app/SpringMVcFormAppApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMVcFormAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMVcFormAppApplication.class, args);
+	}
+
+}
